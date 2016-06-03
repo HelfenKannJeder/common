7b6e35a5d585c21d7c4d47896054b54ff9cd9b5e
Andreas Eberle <email@andreas-eberle.com>
2016-06-03 11:04:55 +0200

    Added CRUD identity methods to IdentityController.

--- identity-provider/src/main/java/de/helfenkannjeder/common/identityprovider/rest/IdentityController.java
+++ identity-provider/src/main/java/de/helfenkannjeder/common/identityprovider/rest/IdentityController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.transaction.Transactional;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 @RestController
 @RequestMapping("identities/")
@@ -19,14 +21,27 @@ public class IdentityController {
 	@Autowired
 	private IdentityService identityService;
 
+	@RequestMapping(method = RequestMethod.POST)
+	public IdentityDto createUser(@Valid IdentityDto identityDto) {
+		Identity identity = IdentityDto.createUser(identityDto);
+		identity = identityService.createIdentity(identity);
+		return IdentityDto.createFullDto(identity);
+	}
+
 	@RequestMapping(value = "{id}", method = RequestMethod.GET)
-	public IdentityDto getUser(@PathVariable Long id) {
+	public IdentityDto getUser(@NotNull @PathVariable("id") Long id) {
 		return IdentityDto.createFullDto(identityService.findById(id));
 	}
 
-	public IdentityDto createUser(IdentityDto identityDto) {
+	@RequestMapping(value = "{id}", method = RequestMethod.PUT)
+	public IdentityDto updateUser(@NotNull @PathVariable("id") Long id, @Valid IdentityDto identityDto) {
 		Identity identity = IdentityDto.createUser(identityDto);
-		identity = identityService.createUser(identity);
+		identity = identityService.updateIdentity(id, identity);
 		return IdentityDto.createFullDto(identity);
+	}
+
+	@RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+	public void deleteUser(@NotNull @PathVariable("id") Long id) {
+		identityService.deleteIdentityById(id);
 	}
 }
--- identity-provider/src/main/java/de/helfenkannjeder/common/identityprovider/service/IdentityService.java
+++ identity-provider/src/main/java/de/helfenkannjeder/common/identityprovider/service/IdentityService.java
@@ -16,70 +16,70 @@ import static java.lang.String.format;
 @Service
 public class IdentityService {
 
-    private final IdentityRepository identityRepository;
+	private final IdentityRepository identityRepository;
 
-    @Autowired
-    public IdentityService(IdentityRepository identityRepository) {
-        this.identityRepository = identityRepository;
-    }
+	@Autowired
+	public IdentityService(IdentityRepository identityRepository) {
+		this.identityRepository = identityRepository;
+	}
 
-    public List<Identity> findAll() {
-        return Lists.newArrayList(identityRepository.findAll());
-    }
+	public List<Identity> findAll() {
+		return Lists.newArrayList(identityRepository.findAll());
+	}
 
-    public Identity findByEmail(String email) {
-        return identityRepository.findByEmail(email);
-    }
+	public Identity findByEmail(String email) {
+		return identityRepository.findByEmail(email);
+	}
 
-    public Identity findById(Long id) {
-        return identityRepository.findOne(id);
-    }
+	public Identity findById(Long id) {
+		return identityRepository.findOne(id);
+	}
 
-    public Identity createUser(Identity identity) {
-        if (identity == null) {
-            return identity;
-        }
+	public Identity createIdentity(Identity identity) {
+		if (identity == null) {
+			return identity;
+		}
 
-        Identity tmp = identityRepository.findByEmail(identity.getEmail());
-        if (tmp != null) {
-            throw new DuplicateResourceException(format("An identity with email %s already exists", identity.getEmail()));
-        }
+		Identity tmp = identityRepository.findByEmail(identity.getEmail());
+		if (tmp != null) {
+			throw new DuplicateResourceException(format("An identity with email %s already exists", identity.getEmail()));
+		}
 
-        return identityRepository.save(identity);
-    }
+		return identityRepository.save(identity);
+	}
 
-    public Identity updateUser(Identity identity) {
-        if (identity == null) {
-            return null;
-        }
+	public Identity updateIdentity(Long id, Identity identity) {
+		if (identity == null) {
+			return null;
+		}
 
-        if (identity.getId() == null) {
-            throw InvalidDataException.forSingleError("id.not.null", null);
-        }
+		if (identity.getId() == null) {
+			throw InvalidDataException.forSingleError("id.not.null", null);
+		}
 
-        Identity tmp = identityRepository.findOne(identity.getId());
-        if (tmp == null) {
-            throw new ConcurrentDeletedException(identity.getId());
-        }
+		Identity storedEntity = identityRepository.findOne(id);
+		if (storedEntity == null) {
+			throw new ConcurrentDeletedException(identity.getId());
+		}
+		storedEntity.update(identity);
 
-        return identityRepository.save(identity);
-    }
+		return identityRepository.save(storedEntity);
+	}
 
-    //TODO should we really delete users or to be able to track historical stuff just soft-delete/deactivate them?
-    public void deleteUserById(Long id) {
-        Identity identity = identityRepository.findOne(id);
-        if (identity == null) {
-            return;
-        }
+	public void deleteIdentityById(Long id) {
+		Identity identity = identityRepository.findOne(id);
+		if (identity == null) {
+			return;
+		}
 
-        identityRepository.delete(id);
-    }
+		identityRepository.delete(id);
+	}
 
-    public void deleteUser(Identity identity) {
-        if (identity == null) {
-            return;
-        }
+	public void deleteIdentity(Identity identity) {
+		if (identity == null) {
+			return;
+		}
 
-        deleteUserById(identity.getId());
-    }
+		deleteIdentityById(identity.getId());
+	}
 }
