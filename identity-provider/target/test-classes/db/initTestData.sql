55add9cc5a26c016793156dcf0ae6fe7e3d22a65
Andreas Eberle <email@andreas-eberle.com>
2016-06-03 10:06:37 +0200

    Removed target folder and added it to .gitignore

--- identity-provider/target/test-classes/db/initTestData.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO user (id, created, updated, version, city, latitude, longitude, street, street_number, zip_code, auth_provider, email, email_verified, external_id, given_name, phone, surname)
-VALUES
-  (1, NOW(), NOW(), 1, NULL, 49.0, 8.4, NULL, NULL, 76133, 'empty', 'test@helfenkannjeder.de', 0, NULL, 'Given Name', '01234567', 'Surname'),
-  (2, NOW(), NOW(), 1, NULL, 49.0, 8.41, NULL, NULL, 76133, 'empty', 'test2@helfenkannjeder.de', 0, NULL, 'Given Name 2', '01234568', 'Surname 2');
