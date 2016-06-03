e90e4e594351239af036b716f30e7ba30a3e4bc9
Valentin Zickner <zickner@querformatik.de>
2016-06-03 13:31:42 +0200

    Implement API class and dto for oauth provider api.

---
+++ oauth-provider-api/src/main/java/de/helfenkannjeder/oauth/provider/api/dto/UserResponseDto.java
@@ -0,0 +1,24 @@
+package de.helfenkannjeder.oauth.provider.api.dto;
+
+/**
+ * @author Valentin Zickner <valentin.zickner@helfenkannjeder.de>
+ */
+public class UserResponseDto {
+
+    private String id;
+
+    public UserResponseDto() {
+    }
+
+    public UserResponseDto(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
---
+++ oauth-provider-api/src/test/java/de/helfenkannjeder/oauth/provider/api/UserApiTest.java
@@ -0,0 +1,59 @@
+package de.helfenkannjeder.oauth.provider.api;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import de.helfenkannjeder.oauth.provider.api.dto.UserRequestDto;
+import de.helfenkannjeder.oauth.provider.api.dto.UserResponseDto;
+import de.helfenkannjeder.oauth.provider.api.util.TestServer;
+import feign.Feign;
+import feign.jackson.JacksonDecoder;
+import feign.jackson.JacksonEncoder;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Valentin Zickner <valentin.zickner@helfenkannjeder.de>
+ */
+public class UserApiTest {
+
+    private UserApi userApi;
+
+    @Before
+    public void setUp() throws Exception {
+        TestServer.startHttpServer();
+        int port = TestServer.getPort();
+
+        ObjectMapper mapper = new ObjectMapper()
+                .setSerializationInclusion(JsonInclude.Include.NON_NULL)
+                .configure(SerializationFeature.INDENT_OUTPUT, true)
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        this.userApi = Feign.builder()
+                .encoder(new JacksonEncoder(mapper))
+                .decoder(new JacksonDecoder(mapper))
+                .target(UserApi.class, "http://localhost:" + port);
+    }
+
+    @Test
+    public void create_withUsernameAndPassword_returnsNewId() throws Exception {
+        TestServer.addResource("/user/create", new TestServer.StringHttpHandler("{\"id\":\"myUserId\"}"));
+        UserResponseDto response = userApi.create(new UserRequestDto("myUser", "myPassword"));
+        assertEquals("myUserId", response.getId());
+    }
+
+    @Test
+    public void update_withUserAndPassword_doesNotFail() throws Exception {
+        TestServer.addResource("/user/myUserId", new TestServer.NoContentHttpHandler());
+        userApi.update("myUserId", new UserRequestDto("myUser", "myPassword"));
+    }
+
+    @Test
+    public void delete_withId_doesNotFail() throws Exception {
+        TestServer.addResource("/user/myUserId", new TestServer.NoContentHttpHandler());
+        userApi.delete("myUserId");
+    }
+}
