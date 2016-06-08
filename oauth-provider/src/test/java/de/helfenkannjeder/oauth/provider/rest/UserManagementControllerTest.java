14f7453be4aed0ab4528a022845226958a964b42
Valentin Zickner <zickner@querformatik.de>
2016-06-08 18:57:40 +0200

    Refactor test methods.

--- oauth-provider/src/test/java/de/helfenkannjeder/oauth/provider/rest/UserManagementControllerTest.java
+++ oauth-provider/src/test/java/de/helfenkannjeder/oauth/provider/rest/UserManagementControllerTest.java
@@ -1,39 +1,16 @@
 package de.helfenkannjeder.oauth.provider.rest;
 
-import de.helfenkannjeder.oauth.provider.MockMvcOAuthLogin;
 import de.helfenkannjeder.oauth.provider.api.OAuthProviderUserManagementApi;
 import de.helfenkannjeder.oauth.provider.api.dto.UserRequestDto;
 import de.helfenkannjeder.oauth.provider.api.dto.UserResponseDto;
-import de.helfenkannjeder.oauth.provider.configuration.OAuth2ProviderApplication;
-import de.helfenkannjeder.oauth.provider.configuration.OAuth2ProviderConfiguration;
 import de.helfenkannjeder.oauth.provider.domain.OAuthUser;
-import de.helfenkannjeder.oauth.provider.domain.repository.OAuthUserRepository;
-import de.helfenkannjeder.oauth.provider.security.OAuthProviderAuthority;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.provider.ClientDetails;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.security.oauth2.provider.TokenRequest;
-import org.springframework.security.oauth2.provider.client.BaseClientDetails;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.context.WebApplicationContext;
 
-import javax.servlet.Filter;
-import java.util.HashMap;
-
-import static java.util.Collections.singleton;
 import static junit.framework.TestCase.assertNotNull;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -43,57 +20,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 /**
  * @author Valentin Zickner <valentin.zickner@helfenkannjeder.de>
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@WebIntegrationTest(randomPort = true)
-@SpringApplicationConfiguration(classes = OAuth2ProviderApplication.class)
 @Transactional
-public class UserManagementControllerTest {
+public class UserManagementControllerTest extends AbstractOAuthControllerTest {
 
     public static final String RESOURCE_PREFIX = "/admin";
-    public static final String DEFAULT_USER = "my-default-user";
-    public static final String DEFAULT_PASSWORD = "my-default-password";
 
-    @Autowired
-    private WebApplicationContext webApplicationContext;
-
-    private MockMvc mockMvc;
     private ObjectMapper objectMapper = new ObjectMapper();
-
-    @Autowired
-    private OAuthUserRepository oAuthUserRepository;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Autowired
-    private Filter springSecurityFilterChain;
-
-    @Autowired
-    private MockMvcOAuthLogin mockMvcOAuthLogin;
 
     private UserRequestDto userRequestDto;
 
-    private Authentication createAuthentication(String grantType, OAuthProviderAuthority... authorities) {
-
-        StringBuilder authoritiesBuilder = new StringBuilder();
-        for (OAuthProviderAuthority authority : authorities) {
-            authoritiesBuilder.append(",").append(authority);
-        }
-        ClientDetails client = new BaseClientDetails("oauth-provider-admin", null, OAuth2ProviderConfiguration.SCOPE_DEFAULT, grantType, authoritiesBuilder.substring(1));
-        return new OAuth2Authentication(new TokenRequest(new HashMap<>(), "oauth-provider-admin", singleton(OAuth2ProviderConfiguration.SCOPE_DEFAULT), grantType).createOAuth2Request(client), null);
-    }
-
     @Before
     public void setUp() throws Exception {
-        this.mockMvc = MockMvcBuilders
-                .webAppContextSetup(this.webApplicationContext)
-                .addFilters(springSecurityFilterChain)
-                .build();
         userRequestDto = new UserRequestDto("my-user", "my-password");
-
-        oAuthUserRepository.save(new OAuthUser(DEFAULT_USER, passwordEncoder.encode(DEFAULT_PASSWORD)));
     }
-
 
     @Test
     public void create_withAdminAndUsernameAndPassword_returnsNewUserId() throws Exception {
@@ -179,11 +118,4 @@ public class UserManagementControllerTest {
         ).andExpect(status().isUnauthorized());
     }
 
-    private String getAuthorizationAdmin() throws Exception {
-        return mockMvcOAuthLogin.getAccessTokenWithClientSecret("oauth-provider-admin", "default-secret");
-    }
-
-    private String getAuthorizationDefaultUser() throws Exception {
-        return mockMvcOAuthLogin.getAccessTokenWithAuthorizationCode("come2help-web", "secret", DEFAULT_USER, DEFAULT_PASSWORD);
-    }
 }
